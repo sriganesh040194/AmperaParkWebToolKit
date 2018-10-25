336947111a96893d8fca48a5dad19a8f347f542e
Sriganesh Karuppannan <sriganesh040194@gmail.com>
2018-10-25 11:54:57 +0200

    deploy testing

--- src/app/service/config/config.service.ts
+++ src/app/service/config/config.service.ts
@@ -10,13 +10,14 @@ export class ConfigService {
   
   
   geIrradiance() {
+    console.log("Fetch Request start");
     var url = "http://re.jrc.ec.europa.eu/pvgis5/DRcalc.php?lat=45&lon=8&month=3&global=1&browser=0";
     
-//     var request = new HttpRequest("GET", url);
-// console.log(request);
-// var response = this.httpClient.request(new HttpRequest("GET", url,)).get(url).subscribe((res)=>{
-//   console.log(res);
-// });
+    var request = new HttpRequest("GET", url);
+console.log(request);
+var response = this.httpClient.request(request).subscribe((res)=>{
+  console.log(res);
+});
 const headers = new Headers();
 headers.append('Accept', 'text/xml');
 
@@ -25,15 +26,15 @@ headers.append('Accept', 'text/xml');
 //   header.append('text/xml');
 
 console.log("Fetch Request start");
-var request= new Request(url,{ method:"GET", mode:"no-cors", headers: {
-  "Content-Type": "application/json; charset=utf-8",
-  // "Content-Type": "application/x-www-form-urlencoded",
-}});
-  fetch(request).then((res)=>{
-// res.formData().then(aa=>console.log(aa));
-//var aaa = res.json();
-    console.log(res);
-  });
+// var request= new Request(url,{ method:"GET", mode:"no-cors", headers: {
+//   "Content-Type": "application/json; charset=utf-8",
+//   // "Content-Type": "application/x-www-form-urlencoded",
+// }});
+//   fetch(request).then((res)=>{
+// // res.formData().then(aa=>console.log(aa));
+// //var aaa = res.json();
+//     console.log(res);
+//   });
 
     //var data = this.http.request()
   }
