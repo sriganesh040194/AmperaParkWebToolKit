a3b23402d988cabbc4b424b69ef330c5804755ab
Sriganesh Karuppannan <sriganesh040194@gmail.com>
2018-10-25 14:01:11 +0200

    Deploy to new server

--- src/app/service/config/config.service.ts
+++ src/app/service/config/config.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpRequest, HttpHeaders } from '@angular/common/http'; 
+import { HttpClient, HttpRequest, HttpHeaders, HttpParams } from '@angular/common/http'; 
 
 @Injectable({
   providedIn: 'root'
@@ -10,31 +10,40 @@ export class ConfigService {
   
   
   geIrradiance() {
-    console.log("Fetch Request start");
+
+    
+const headers = new Headers();
+headers.append("Content-Type","text/html"); 
+headers.append("responseType", "text")
+    console.log("HTTP Request start");
     var url = "http://re.jrc.ec.europa.eu/pvgis5/DRcalc.php?lat=45&lon=8&month=3&global=1&browser=0";
     
-    var request = new HttpRequest("GET", url);
+    var request = new HttpRequest("GET", url,headers);
 console.log(request);
+// var param = new HttpParams().append("responseType","text");
+console.log("HTTP Client Get");
+var aa = this.httpClient.get(url).subscribe(res=>{
+  console.log("HTTP inside get response");
+  console.log(res)});
+   console.log("HTTP Client");
 var response = this.httpClient.request(request).subscribe((res)=>{
   console.log(res);
 });
-const headers = new Headers();
-headers.append('Accept', 'text/xml');
 
 // var meta = { "Content-Type": "text/xml" }
 //   var header = new Headers();
 //   header.append('text/xml');
 
 console.log("Fetch Request start");
-// var request= new Request(url,{ method:"GET", mode:"no-cors", headers: {
-//   "Content-Type": "application/json; charset=utf-8",
-//   // "Content-Type": "application/x-www-form-urlencoded",
-// }});
-//   fetch(request).then((res)=>{
-// // res.formData().then(aa=>console.log(aa));
-// //var aaa = res.json();
-//     console.log(res);
-//   });
+var requestw= new Request(url,{ method:"GET", mode:"no-cors", headers: {
+  "Content-Type": "text/html",
+  // "Content-Type": "application/x-www-form-urlencoded",
+}});
+  fetch(requestw).then((res)=>{
+// res.formData().then(aa=>console.log(aa));
+//var aaa = res.json();
+    console.log(res);
+  });
 
     //var data = this.http.request()
   }
